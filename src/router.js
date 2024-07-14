4f3ad51aef0cb3fe7da0402ae4383e284592c93c
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-07-14 02:29:47 -0500

    router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from './views/DashboardView.vue';
 import Tickets from './views/Tickets.vue';
-import NewTicket from './views/NewTicketView.vue';
+import Evento from './views/Evento.vue';
 import Projects from './views/ProjectsView.vue';
 import Users from './views/UsersView.vue';
 import Login from './views/LoginView.vue'; // Renombrado para asegurar un nombre multi-palabra
@@ -12,6 +12,10 @@ import CrearPersonal from './components/personal.components/crear.vue'
 import EditarPersonal from './components/personal.components/editar.vue'
 import ListarPersonal from './views/Personal.vue'
 
+import ListarEventos from './views/Evento.vue'
+import NewEvento from './components/evento.components/crear.vue'
+import EditarEvento from './components/evento.components/editar.vue'
+
 const routes = [
   {
     path: '/',
@@ -19,14 +23,19 @@ const routes = [
     children: [
       { path: '', component: Dashboard },
       { path: 'tickets', component: Tickets },
-      { path: 'new-ticket', component: NewTicket },
+      { path: 'evento', component: Evento },
       { path: 'projects', component: Projects },
       { path: 'users', component: Users },
 
       //rutas de personal
   { path: '/personal/crear', component: CrearPersonal },
   { path: '/personal/editar/:id',name:'EditarPersonal', component: EditarPersonal},
-  { path: '/personal', component: ListarPersonal }
+  { path: '/personal', component: ListarPersonal },
+
+
+  { path: '/eventos', component: ListarEventos },
+  { path: '/eventos/crear', component: NewEvento },
+  { path: '/eventos/editar/:id', name: 'EditarEvento', component: EditarEvento }
     ],
   },
   { path: '/login', component: Login },

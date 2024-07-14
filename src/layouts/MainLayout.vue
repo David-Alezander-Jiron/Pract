afa21afc0673c408f7315347b1df8dfc05e85c9a
Edison20F <ela.flores@yavirac.edu.ec>
2024-07-14 02:14:48 -0500

    2

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,24 +1,23 @@
 <template>
-    <div>
-      
-      <SidebarComponent />
-      <router-view />
-    </div>
-  </template>
-  
-  <script>
-  
-  import SidebarComponent from '@/components/SidebarComponent.vue';
-  
-  export default {
-    components: {
-      
-      SidebarComponent,
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Puedes añadir estilos específicos para el layout aquí */
-  </style>
-  
+  <div>
+
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  components: {
+
+
+  },
+};
+</script>
+
+<style scoped>
+/* Puedes añadir estilos específicos para el layout aquí */
+</style>

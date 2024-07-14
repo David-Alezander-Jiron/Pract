fecf900c4a08cab901ed7e8957fab809f148a245
Edison20F <ela.flores@yavirac.edu.ec>
2024-07-14 00:27:34 -0500

    personal

--- src/router.js
+++ src/router.js
@@ -7,6 +7,11 @@ import Users from './views/UsersView.vue';
 import Login from './views/LoginView.vue'; // Renombrado para asegurar un nombre multi-palabra
 import MainLayout from './layouts/MainLayout.vue';
 
+//rutas de personal
+import CrearPersonal from './components/personal.components/crear.vue'
+import EditarPersonal from './components/personal.components/editar.vue'
+import ListarPersonal from './views/Personal.vue'
+
 const routes = [
   {
     path: '/',
@@ -17,6 +22,11 @@ const routes = [
       { path: 'new-ticket', component: NewTicket },
       { path: 'projects', component: Projects },
       { path: 'users', component: Users },
+
+      //rutas de personal
+  { path: '/personal/crear', component: CrearPersonal },
+  { path: '/personal/editar/:id',name:'EditarPersonal', component: EditarPersonal},
+  { path: '/personal', component: ListarPersonal }
     ],
   },
   { path: '/login', component: Login },

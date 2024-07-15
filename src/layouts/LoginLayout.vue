d983bde0dc2dacc87613f0033ac704f8c8ff5015
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-07-14 19:40:57 -0500

    Add files via upload

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login-layout">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <style scoped>
+  .login-layout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f8f9fa; /* Color de fondo opcional */
+  }
+  </style>
+  

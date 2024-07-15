2c61212aeaacce6f48988c9961d52d3dee59efef
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-07-14 19:41:55 -0500

    NavbarComponent.vue

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,20 +1,25 @@
 <template>
-  <nav class="navbar">
-    <ul>
-      <li>
-        <input type="text" placeholder="Buscar...">
-        <button type="button" class="icon-button">
-          <i class="fas fa-search"></i>
-        </button>
-        <button type="button" class="icon-button">
-          <i class="fas fa-bell"></i>
-        </button>
-        <router-link to="/login" class="icon-button">
-          <i class="fas fa-user-circle"></i>
-          <span>Login</span>
-        </router-link>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Navbar</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <input type="text" class="form-control" placeholder="Buscar...">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-bell"></i></a>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link"><i class="fas fa-user-circle"></i> Administrador</router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -23,51 +28,3 @@ export default {
   name: 'NavbarComponent'
 };
 </script>
-
-<style>
-.navbar {
-  background-color: #f8f9fa;
-  padding: 10px;
-}
-
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-
-li {
-  display: flex;
-  align-items: center;
-}
-
-input {
-  margin-left: auto;
-  padding: 5px;
-  position: relative;
-  left: 870px;
-}
-
-.icon-button {
-  background: none;
-  border: none;
-  margin-left: 10px;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  position: relative;
-  left: 800px;
-}
-
-.icon-button i {
-  font-size: 1.2em;
-  margin-right: 5px;
-}
-
-.icon-button span {
-  margin-left: 5px;
-  font-size: 1em;
-  position: relative;
-  left: 5px;
-}
-</style>

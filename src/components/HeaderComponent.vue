e436e8b1629ff655271d00addb0934dac217c12c
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-07-14 19:42:11 -0500

    HeaderComponent.vue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,52 +1,26 @@
-<!-- src/components/Topbar.vue -->
 <template>
-  <div class="menu-below">
-    <ul>
-      <li><router-link to="/">Dashboard/</router-link></li>
-      <li><router-link to="/usuarios">Usuarios/</router-link></li>
-      <li><router-link to="/">Home/</router-link></li>
-    </ul>
-  </div>
+  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">EventTix</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <div class="navbar-nav">
+      <div class="nav-item text-nowrap">
+        <a class="nav-link px-3" href="#">Sign out</a>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'menu-below'
+  name: 'HeaderComponent'
 };
 </script>
 
 <style scoped>
-.menu-below {
-  margin-top: 60px; /* Ajusta este valor para mover el menú más arriba o más abajo */
-  margin-left: 250px; /* Ajusta el margen izquierdo para alinear con la barra lateral */
-  padding: 10px 20px;
-  background-color: #f8f9fa;
-  border-bottom: 1px solid #dee2e6;
-  display: flex;
-  position: relative;
-  right: 220px;
-  bottom: 80px;
-  width: 1094px;
-}
-
-.menu-below ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  gap: 20px;
-}
-
-.menu-below ul li {
-  padding: 10px 0;
-}
-
-.menu-below ul li a {
-  text-decoration: none;
-  color: #000;
-}
-
-.menu-below ul li a:hover {
-  text-decoration: underline;
+.navbar {
+  margin-bottom: 20px;
 }
 </style>
